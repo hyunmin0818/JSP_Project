2178eb04de2f5bbd8851e4fff82a503ca02ac632
임현민 <120604k@gmail.com>
2024-03-13 11:50:38 +0900

    movieParsing.java delete

--- src/main/java/com/movie/web/api/movie.java
+++ src/main/java/com/movie/web/api/movie.java
@@ -9,72 +9,77 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class movie {
+
     public static void main(String[] args) {
-	
-    	// 인증키 (개인이 받아와야함)
-    	String key = "H594OAKW457U2H270K93";
+    
+        // 인증키 (개인이 받아와야 함)
+        String key = "H594OAKW457U2H270K93";
 
-    	// 파싱한 데이터를 저장할 변수
-    	String result = "";
+        // 파싱한 데이터를 저장할 변수
+        String result = "";
 
-    	try {
+        try {
 
-    		URL url = new URL("http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2"
-    				+ "&listCount=500&ServiceKey=" + key );
+            // API 요청 URL 생성
+            URL url = new URL("http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2"
+                    + "&listCount=500&ServiceKey=" + key);
 
-    		BufferedReader bf;
+            // API 응답 데이터 읽기
+            BufferedReader bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+            result = bf.readLine();
 
-    		bf = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+            // JSON 파서 초기화
+            JSONParser jsonParser = new JSONParser();
+            
+            // JSON 데이터를 파싱하여 JSONObject로 변환
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
+            JSONObject movieInfoResult = (JSONObject) jsonObject.get("movieInfoResult");
+            JSONObject movieInfo = (JSONObject) movieInfoResult.get("movieInfo");
 
-    		result = bf.readLine();
+            // 필요한 정보 추출
+            JSONArray nations = (JSONArray) movieInfo.get("nations");
+            JSONObject nations_nationNm = (JSONObject) nations.get(0);
 
-        	JSONParser jsonParser = new JSONParser();
-        	
-        	JSONObject jsonObject = (JSONObject)jsonParser.parse(result);
-        	JSONObject movieInfoResult = (JSONObject)jsonObject.get("movieInfoResult");
-        	JSONObject movieInfo = (JSONObject)movieInfoResult.get("movieInfo");
+            JSONArray directors = (JSONArray) movieInfo.get("directors");
+            JSONObject directors_peopleNm = (JSONObject) directors.get(0);
 
-        	JSONArray nations = (JSONArray)movieInfo.get("nations");
-        	JSONObject nations_nationNm = (JSONObject)nations.get(0);
+            JSONArray genres = (JSONArray) movieInfo.get("genres");
 
-        	JSONArray directors = (JSONArray)movieInfo.get("directors");
-        	JSONObject directors_peopleNm = (JSONObject)directors.get(0);
+            JSONArray actors = (JSONArray) movieInfo.get("actors");
 
-        	JSONArray genres = (JSONArray)movieInfo.get("genres");
+            // 출력
+            System.out.println("영화코드 : " + movieInfo.get("movieCd"));
+            System.out.println("영화명(한글) : " + movieInfo.get("movieNm"));
+            System.out.println("영화명(영문) : " + movieInfo.get("movieNmEn"));
+            System.out.println("재생시간 : " + movieInfo.get("showTm"));
+            System.out.println("개봉일 : " + movieInfo.get("openDt"));
+            System.out.println("영화유형 : " + movieInfo.get("typeNm"));
+            System.out.println("제작국가명 : " + nations_nationNm.get("nationNm"));
 
-        	JSONArray actors = (JSONArray)movieInfo.get("actors");
+            String genreNm = "";
 
-        	System.out.println("영화코드 : " + movieInfo.get("movieCd"));
-        	System.out.println("영화명(한글) : " + movieInfo.get("movieNm"));
-        	System.out.println("영화명(영문) : " + movieInfo.get("movieNmEn"));
-        	System.out.println("재생시간 : " + movieInfo.get("showTm"));
-        	System.out.println("개봉일 : " + movieInfo.get("openDt"));
-        	System.out.println("영화유형 : " + movieInfo.get("typeNm"));
-        	System.out.println("제작국가명 : " + nations_nationNm.get("nationNm"));
-       	
-        	String genreNm = "";
-       	
-        	for(int i = 0; i < genres.size(); i++) {
-            	JSONObject genres_genreNm = (JSONObject)genres.get(i);
-            	genreNm += genres_genreNm.get("genreNm") + " ";
-        	}
+            // 장르 정보 추출
+            for (int i = 0; i < genres.size(); i++) {
+                JSONObject genres_genreNm = (JSONObject) genres.get(i);
+                genreNm += genres_genreNm.get("genreNm") + " ";
+            }
 
-        	System.out.println("장르 : " + genreNm);
-       	
-        	System.out.println("감독명 : " + directors_peopleNm.get("peopleNm"));
-       	
-        	String peopleNm = "";
-       	
-        	for(int i = 0; i < actors.size(); i++) {
-        		JSONObject actors_peopleNm = (JSONObject)actors.get(i);
-        		peopleNm += actors_peopleNm.get("peopleNm") + " ";
-        		peopleNm = peopleNm + actors_peopleNm.get("peopleNm") + " ";
-        	}
-       	
-        	System.out.println("출연배우 : " + peopleNm);
+            System.out.println("장르 : " + genreNm);
 
-    	}catch(Exception e) {
-    		e.printStackTrace();
-    	}
+            System.out.println("감독명 : " + directors_peopleNm.get("peopleNm"));
+
+            String peopleNm = "";
+
+            // 출연 배우 정보 추출
+            for (int i = 0; i < actors.size(); i++) {
+                JSONObject actors_peopleNm = (JSONObject) actors.get(i);
+                peopleNm += actors_peopleNm.get("peopleNm") + " ";
+            }
+
+            System.out.println("출연배우 : " + peopleNm);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
