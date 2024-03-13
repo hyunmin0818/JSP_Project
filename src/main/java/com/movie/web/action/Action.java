69f2a2d39f3c1430334aa213c9ec17785aceeee7
임현민 <120604k@gmail.com>
2024-03-13 18:28:39 +0900

    config id pw 조정

--- src/main/java/com/movie/web/action/Action.java
+++ src/main/java/com/movie/web/action/Action.java
@@ -3,6 +3,7 @@ package com.movie.web.action;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+
 public interface Action {
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response );
 	
