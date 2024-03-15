b216864f034fe8738cdeff9eff52a33b56255b1a
game4eyes <darkjjun68@gmail.com>
2024-03-15 17:03:29 +0900

    lieksDAO 수정

--- src/main/java/com/movie/web/dao/LikesDAO.java
+++ src/main/java/com/movie/web/dao/LikesDAO.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 
+import com.movie.web.dto.LikesDTO;
+import com.movie.web.dto.MovieDTO;
 import com.movie.web.mybatis.SqlMapConfig;
 
 public class LikesDAO {
@@ -17,21 +19,30 @@ public class LikesDAO {
         this.sqlSession = sqlSession;
     }
 
-    // 영화 좋아요 수 증가
-    public void incrementMovieLikes(String movieSeq) {
-        try {
-            sqlSession.update("Movie.incrementMovieLikes", movieSeq);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    // 영화 좋아요 추가 및 좋아요 수 업데이트
+    public boolean addLikeAndUpdateLikesCount(LikesDTO ldto) {
+        int result = sqlSession.update("Movie.UpdateLikesCount", ldto);
+        return result > 0;
     }
 
-    // 영화 좋아요 수 감소
-    public void decrementMovieLikes(String movieSeq) {
-        try {
-            sqlSession.update("Movie.decrementMovieLikes", movieSeq);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    // 영화 좋아요 취소
+    public boolean removeLike(LikesDTO ldto) {
+        int result = sqlSession.delete("Movie.removeLikeAndUpdateLikesCount", ldto);
+        return result > 0;
     }
+
+    // 특정 영화에 대한 좋아요 수 조회
+    public int getLikesCount(String movieSeq) {
+        return sqlSession.selectOne("Movie.getLikesCount", movieSeq);
+    }
+    
+    // 영화 좋아요 수를 내림차순으로 페이징하여 가져오기
+    public List<Integer> getLikesCountDescPaging(int start, int offset) {
+        Map<String, Integer> params = new HashMap<>();
+        params.put("start", start);
+        params.put("offset", offset);
+        return sqlSession.selectList("Movie.getLikesCountDescPaging", params);
+    }
+    
+    
 }
